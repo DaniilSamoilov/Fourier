3d75d13a71a3aecab0b4515319933176a02f3c4d
DaniilSamoilov <danya.sam2@yandex.ru>
2024-06-03 14:21:32 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Fourier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.FurierBtn = new System.Windows.Forms.Button();
+            this.SelectedFunction = new System.Windows.Forms.ComboBox();
+            this.NCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.periodCount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(776, 371);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // FurierBtn
+            // 
+            this.FurierBtn.Location = new System.Drawing.Point(699, 405);
+            this.FurierBtn.Name = "FurierBtn";
+            this.FurierBtn.Size = new System.Drawing.Size(75, 23);
+            this.FurierBtn.TabIndex = 1;
+            this.FurierBtn.Text = "Разложить";
+            this.FurierBtn.UseVisualStyleBackColor = true;
+            this.FurierBtn.Click += new System.EventHandler(this.FurierBtn_Click);
+            // 
+            // SelectedFunction
+            // 
+            this.SelectedFunction.FormattingEnabled = true;
+            this.SelectedFunction.Location = new System.Drawing.Point(146, 405);
+            this.SelectedFunction.Name = "SelectedFunction";
+            this.SelectedFunction.Size = new System.Drawing.Size(187, 21);
+            this.SelectedFunction.TabIndex = 2;
+            // 
+            // NCount
+            // 
+            this.NCount.Location = new System.Drawing.Point(584, 392);
+            this.NCount.Name = "NCount";
+            this.NCount.Size = new System.Drawing.Size(100, 20);
+            this.NCount.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "F(x)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(455, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Количество разбиений";
+            // 
+            // periodCount
+            // 
+            this.periodCount.Location = new System.Drawing.Point(584, 418);
+            this.periodCount.Name = "periodCount";
+            this.periodCount.Size = new System.Drawing.Size(100, 20);
+            this.periodCount.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 421);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Количество отображаемых периодов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.periodCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NCount);
+            this.Controls.Add(this.SelectedFunction);
+            this.Controls.Add(this.FurierBtn);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button FurierBtn;
+        private System.Windows.Forms.ComboBox SelectedFunction;
+        private System.Windows.Forms.TextBox NCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox periodCount;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static Integral.IntegralMethods;
+
+namespace Fourier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SelectedFunction.Items.AddRange(Functions.Keys.ToArray());
+        }
+
+        private void FurierBtn_Click(object sender, EventArgs e)
+        {
+            DrawGraph(Functions[SelectedFunction.Text], Periods[SelectedFunction.Text]);
+            Furier(Functions[SelectedFunction.Text], Periods[SelectedFunction.Text],Convert.ToInt32(NCount.Text),1000);
+            chart1.Legends.Clear();
+            var legend = new Legend
+            {
+                Docking = Docking.Top,
+                Alignment = StringAlignment.Center
+            };
+            chart1.Legends.Add(legend);
+            chart1.Series["OriginalFunction"].LegendText = "Изначальная функция";
+            chart1.Series["FourierSeries"].LegendText = "Ряд Фурье";
+        }
+        public void Furier(Func<double, double> func, double period, int nTerms, int points)
+        {
+            int N = points; // Количество точек для построения графика
+            double dt = period / N;
+            double[] a = new double[nTerms];
+            double[] b = new double[nTerms];
+            int numPeriods = Convert.ToInt32(periodCount.Text); // количество отображаемых периодов
+
+            // Вычисление коэффициентов Фурье с использованием метода Симпсона
+            for (int n = 0; n < nTerms; n++)
+            {
+                // Вычисление коэффициентов a_n
+                Func<double, double> anFunction = x => func(x) * Math.Cos(2 * Math.PI * n * x / period);
+                a[n] = -Simpson(anFunction, 0, period, N) * 2 / period;
+
+                // Вычисление коэффициентов b_n
+                Func<double, double> bnFunction = x => func(x) * Math.Sin(2 * Math.PI * n * x / period);
+                b[n] = -Simpson(bnFunction, 0, period, N) * 2 / period;
+            }
+
+            // Построение графика ряда Фурье
+            var fourierSeries = new Series
+            {
+                Name = "FourierSeries",
+                Color = System.Drawing.Color.Blue,
+                ChartType = SeriesChartType.Line,
+                BorderWidth = 3
+            };
+
+            for (int k = 0; k <= N * numPeriods; k++)
+            {
+                double t = k * dt;
+                double sum = a[0] / 2.0; // первый член (a0 / 2)
+                for (int n = 1; n < nTerms; n++)
+                {
+                    sum += a[n] * Math.Cos(2 * Math.PI * n * t / period) + b[n] * Math.Sin(2 * Math.PI * n * t / period);
+                }
+                fourierSeries.Points.AddXY(t, sum);
+            }
+
+            chart1.Series.Add(fourierSeries);
+            chart1.ChartAreas[0].RecalculateAxesScale();
+        }
+
+
+
+
+
+        private void DrawGraph(Func<double, double> func, double period)
+        {
+            chart1.Series.Clear();
+
+            var series = new Series
+            {
+                Name = "OriginalFunction",
+                Color = System.Drawing.Color.Red,
+                ChartType = SeriesChartType.Line,
+                BorderWidth = 10
+            };
+
+            double step = period / 100; // шаг для построения графика
+            int numPeriods = Convert.ToInt32(periodCount.Text); // количество отображаемых периодов
+
+            for (double x = 0; x <= period * numPeriods; x += step)
+            {
+                series.Points.AddXY(x, func(x));
+            }
+
+            chart1.Series.Add(series);
+            chart1.ChartAreas[0].RecalculateAxesScale();
+        }
+
+
+        public static Dictionary<string, Func<double, double>> Functions = new Dictionary<string, Func<double, double>>
+        {
+            {"sin(x)",(x)=>Math.Sin(x) },
+            {"cos(x)",(x)=>Math.Cos(x) },
+            {"sin(x)+cos(3x)", (x) => Math.Sin(x) + Math.Cos(3*x) },
+            {"sin(x)*sin(x/2)",(x) => Math.Sin(x) + Math.Sin(x/2) },
+            {"alternating", (x) => (Math.Floor(x) % 2 == 0) ? 1 : -1 }, // Функция чередования
+            {"square wave", (x) => (Math.Floor(2 * x / Math.PI) % 2 == 0) ? 1 : -1 }, // Квадратная волна
+            {"sawtooth wave", (x) => 2 * (x / Math.PI - Math.Floor(x / Math.PI + 0.5)) }, // Пилообразная волна
+            {"triangle wave", (x) => Math.Abs(2 * (x / Math.PI - Math.Floor(x / Math.PI + 0.5))) * 2 - 1 }, // Треугольная волна
+            {"sin(x) + cos(2x) + sin(3x)", (x) => Math.Sin(x) + Math.Cos(2 * x) + Math.Sin(3 * x) },
+            {"sin(x) + cos(5x) + sin(7x)", (x) => Math.Sin(x) + Math.Cos(5 * x) + Math.Sin(7 * x) }
+        };
+        public static Dictionary<string, double> Periods = new Dictionary<string, double>
+        {
+            { "sin(x)", 2 * Math.PI },
+            { "cos(x)", 2 * Math.PI },
+            {"sin(x)+cos(3x)",2*Math.PI },
+            {"sin(x)*sin(x/2)",Math.PI*4 },
+            { "alternating", 2 }, // Период функции чередования
+            { "square wave", Math.PI }, // Период квадратной волны
+            { "sawtooth wave", Math.PI }, // Период пилообразной волны
+            { "triangle wave", Math.PI }, // Период треугольной волны
+            { "sin(x) + cos(2x) + sin(3x)", 2 * Math.PI },
+            { "sin(x) + cos(5x) + sin(7x)", 2 * Math.PI }
+        };
+        
+    }
+}
